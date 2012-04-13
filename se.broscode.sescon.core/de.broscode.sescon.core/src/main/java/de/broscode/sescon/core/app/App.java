431aa135a727861366559e042dbce97265ad2215
opollon <nicolaus.walter@gmx.de>
2012-04-13 15:52:34 +0200

    Maven dependencies

---
+++ se.broscode.sescon.core/de.broscode.sescon.core/src/main/java/de/broscode/sescon/core/app/App.java
@@ -0,0 +1,12 @@
+package de.broscode.sescon.core.app;
+
+public class App {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ se.broscode.sescon.core/de.broscode.sescon.core/src/main/test/de/broscode/sescon/core/app/AppTest.java
@@ -0,0 +1,13 @@
+package de.broscode.sescon.core.app;
+
+public class AppTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
